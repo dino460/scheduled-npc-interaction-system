a640fd2012b6ff2ab59a45f8da31de470612234a
dino460 <raphael.z.cali@gmail.com>
2024-04-21 20:51:11 -0300

    preliminary test in the main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct Personality {
+    aggressiveness : Option<f32>,
+    empathy : Option<f32>,
+    lazyness : Option<f32>
+}
+
+struct Person {
+    personality : Personality
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test = Person {
+        personality : Personality {
+            aggressiveness : Some(0.1),
+            empathy : Some(0.8),
+            lazyness : Some(-0.3)
+        }
+    };
+
+    println!("{:?}", test.personality.aggressiveness.unwrap());
 }

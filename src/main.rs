40f692feeccf40f31fce806f250efaf4d4a40edf
dino460 <raphael.z.cali@gmail.com>
2024-05-14 21:36:27 -0300

    Reorganized code to receive input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod matrix_generator;
 use colored::*;
+use std::io;
 
 
 const MATRIX_SIZE : usize = 50;
@@ -18,45 +19,61 @@ struct World {
     world         : Vec<Vec<usize>>
 }
 
+
 fn main() {
 
     //                              [0, 1]
-    let path_distribution_weights = [2, 1];
     let job_distribution_weights  = [4, 1];
-
-    let mut world : World = World {
-        paths : matrix_generator::random_matrix_with_weights(path_distribution_weights.to_vec(), MATRIX_SIZE),
-        altered_paths : (0..MATRIX_SIZE).map(|_| { (0..MATRIX_SIZE).map(|_| 0).collect()}).collect(),
-        jobs : matrix_generator::random_matrix_with_weights(job_distribution_weights.to_vec(), MATRIX_SIZE),
-        world : (0..MATRIX_SIZE).map(|_| { (0..MATRIX_SIZE).map(|_| 0).collect()}).collect()
-    };
-
-    /*
-        Does the smoothing process in a predetermined number of passes
-        Does not directly alter paths, only uses it as base
-        altered_paths is the final matrix with the smoothing results
-    */
-    world.altered_paths = matrix_generator::smooth_binary_matrix_ones(&world.paths, SMOOTH_PASS_MAX);
+    let path_distribution_weights = [2, 1];
     
-    world.world = matrix_generator::sum_matrices(&world.altered_paths, &world.world);
+    let mut input : String = "".to_string();
 
-    
-    // * Print the generated matrices for better visualization
-    println!();
-    println!("{}", "World matrix");
-    for i in 0..world.world.len() {
-        for j in 0..world.world.len() {
-            match world.world[i][j] {
-                0 => print!("{} ", world.world[i][j].to_string().red()),
-                1 => print!("{} ", world.world[i][j].to_string().green()),
-                2 => print!("{} ", world.world[i][j].to_string().blue()),
-                3 => print!("{} ", world.world[i][j].to_string().yellow()),
-                4 => print!("{} ", world.world[i][j].to_string().magenta()),
-                _ => print!("{} ", world.world[i][j])
+    while input.to_lowercase() != "q".to_string() {
+        input.clear();
+        io::stdin().read_line(&mut input).expect("failed to read line");
+        input.pop();
+
+        if input.to_lowercase() == "generate".to_string() {
+            let mut world : World = World {
+                paths : matrix_generator::random_matrix_with_weights(path_distribution_weights.to_vec(), MATRIX_SIZE),
+                altered_paths : (0..MATRIX_SIZE).map(|_| { (0..MATRIX_SIZE).map(|_| 0).collect()}).collect(),
+                jobs : matrix_generator::random_matrix_with_weights(job_distribution_weights.to_vec(), MATRIX_SIZE),
+                world : (0..MATRIX_SIZE).map(|_| { (0..MATRIX_SIZE).map(|_| 0).collect()}).collect()
+            };
+        
+            /*
+                Does the smoothing process in a number of passes
+                Does not directly alter paths, only uses it as base
+                altered_paths is the final matrix with the smoothing results
+            */
+            world.altered_paths = matrix_generator::smooth_binary_matrix_ones(&world.paths, SMOOTH_PASS_MAX);
+            
+            world.world = matrix_generator::sum_matrices(&world.altered_paths, &world.world);
+        
+            
+            // * Print the generated matrices for better visualization
+            println!();
+            println!("{}", "World matrix");
+            for i in 0..world.world.len() {
+                for j in 0..world.world.len() {
+                    match world.world[i][j] {
+                        0 => print!("{} ", world.world[i][j].to_string().red()),
+                        1 => print!("{} ", world.world[i][j].to_string().green()),
+                        2 => print!("{} ", world.world[i][j].to_string().blue()),
+                        3 => print!("{} ", world.world[i][j].to_string().yellow()),
+                        4 => print!("{} ", world.world[i][j].to_string().magenta()),
+                        _ => print!("{} ", world.world[i][j])
+                    }
+                }
+                println!();
             }
+        
+            println!();
+
+        } else if input.to_lowercase() == "save".to_string() {
+            println!("Still to implement");
         }
-        println!();
     }
 
-    println!();
+
 }
